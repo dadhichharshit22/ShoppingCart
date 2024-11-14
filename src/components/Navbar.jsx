fad5983241b51461966fcd9b740b86a1c321b6d9
dadhichharshit22 <dadhich.harshit22122002@gmail.com>
2024-11-14 10:47:03 +0530

    Initial Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      hello world this is my navbar
+    <div className="bg-blue-500">
+      <div className="flex flex-row justify-between">
+        <NavLink to="/">
+          <div>
+            <img src="./logo.png" alt="" />
+          </div>
+        </NavLink>
+        <div>
+          <NavLink to="/">
+            <p>Home</p>
+          </NavLink>
+          <NavLink to="/cart">
+            <div>
+              <FaShoppingCart />
+            </div>
+          </NavLink>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
